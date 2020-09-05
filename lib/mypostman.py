fd330e857ab61793c8fa7cfd928aed0275abbf6a
Ishan Dutta <ishandutta2007@gmail.com>
2020-09-05 14:16:52 +0530

    responses

--- lib/mypostman.py
+++ lib/mypostman.py
@@ -27,7 +27,7 @@ def load(l):
 class Postman:
     def __init__(self):
         self.num = 0
-        self.keywords = ["facebook", "fbcdn"]
+        self.keywords = ["facebook", "fbcdn", "ip-api", "instagram"]
         self.folder_dict = {}
 
     def request(self, flow):
@@ -74,7 +74,7 @@ class Postman:
             elif "json" in headers.get("Content-Type", ""):
                 is_json = True
 
-        print("DATA:")
+        print("REQUEST DATA:")
         print(data)
         req = Request(
             name=path,
@@ -111,6 +111,39 @@ class Postman:
                 self.folder_dict[folder_name] = folder
             folder.add_request(req)
         self.collection.save_to_file()
+
+    def response(self, flow):
+        print("RESPONSE NO:", self.num)
+        is_present = False
+        for keyword in self.keywords:
+            if keyword in flow.request.host:
+                is_present = True
+                break
+        if not is_present:
+            return
+        # print(flow.response)
+        response = flow.response
+        print("response.content:", response.content[:100])
+        print("response.cookies:", response.cookies)
+        # decode(strict=True)[source]
+        # encode(e)[source]
+        # get_content(strict: bool = True) → bytes[source]
+        # get_text(strict: bool = True) → typing.Union[str, NoneType][source]
+        print("response.headers:", response.headers)
+        print("response.http_version:", response.http_version)
+        print("response.raw_content:", response.raw_content[:100])
+        print("response.reason:", response.reason)
+        # refresh(now=None)[source]
+        # replace(pattern, repl, flags=0, count=0)[source]
+        print("response.status_code:", response.status_code)
+        print("response.text:", response.text[:100])
+        print("response.timestamp_end:", response.timestamp_end)
+        print("response.timestamp_start:", response.timestamp_start)
+        self.collectionofresponses = CollectionOfResponses.getInstance(
+            response_no=self.num
+        )
+        self.collectionofresponses.add_response(response)
+        self.collectionofresponses.save_to_file()
 
     @staticmethod
     def get_path(req):
@@ -222,11 +255,13 @@ class Collection(object):
         :return: None
         """
         obj = self.serialize()
-        if not os.path.exists("output/" + self.name):
-            os.makedirs("output/" + self.name)
+        if not os.path.exists("output/{}/postman_collections/".format(self.name)):
+            os.makedirs("output/{}/postman_collections/".format(self.name))
 
         filename = "{file_name}.json".format(**{"file_name": self.name})
-        with open("output/" + self.name + "/" + filename, "wt") as f:
+        with open(
+            "output/{}/postman_collections/".format(self.name) + "/" + filename, "wt"
+        ) as f:
             json.dump(obj, f, indent=4, ensure_ascii=False)
         print("save Collection to file:", self.name)
 
@@ -312,6 +347,141 @@ class Request(object):
         return obj
 
 
+class Response(object):
+    def __init__(
+        self, name, response, response_no=None,
+    ):
+        """
+        A Response object represents a single Postman Response
+        :param name: Name of the response
+        :param response: URL to send the response
+        :param response_no: HTTP method
+        """
+        print("ResponseResponseResponseResponseResponseResponseRequest:")
+        print(object)
+        if response_no:
+            self.id = str(response_no).zfill(4) + "_" + str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+
+        self.name = name
+        # self.response = response
+
+        content = response.content
+        print("response.content:", response.content)
+
+        cookies = response.cookies
+        print("response.cookies:", response.cookies)
+        # decode(strict=True)[source]
+        # encode(e)[source]
+        # get_content(strict: bool = True) → bytes[source]
+        # get_text(strict: bool = True) → typing.Union[str, NoneType][source]
+        headers = response.headers
+        print("response.headers:", response.headers)
+
+        http_version = response.http_version
+        print("response.http_version:", response.http_version)
+
+        raw_content = response.raw_content
+        print("response.raw_content:", response.raw_content)
+
+        reason = response.reason
+        print("response.reason:", response.reason)
+        # refresh(now=None)[source]
+        # replace(pattern, repl, flags=0, count=0)[source]
+
+        status_code = response.status_code
+        print("response.status_code:", response.status_code)
+
+        text = response.text
+        print("response.text:", response.text)
+
+        timestamp_end = response.timestamp_end
+        print("response.timestamp_end:", response.timestamp_end)
+
+        timestamp_start = response.timestamp_start
+        print("response.timestamp_start:", response.timestamp_start)
+
+    def serialize(self):
+        """
+        Serialize Response object
+        :return: Response dict
+        """
+        # headers = {} if self.headers is None else self.headers
+        obj = OrderedDict()
+        obj["id"] = self.id
+        obj["name"] = self.name
+
+        try:
+            obj["content"] = self.content
+        except Exception as e:
+            obj["content"] = ""
+
+        try:
+            obj["cookies"] = self.cookies
+        except Exception as e:
+            obj["cookies"] = []
+
+        # obj["headers"] = self.headers
+        try:
+            obj["headers"] = "".join("%s: %s\n" % kv for kv in self.headers.items())
+        except Exception as e:
+            obj["headers"] = {}
+
+        try:
+            obj["http_version"] = self.http_version
+        except Exception as e:
+            obj["http_version"] = ""
+
+        try:
+            obj["raw_content"] = json.dumps(self.raw_content, indent=4)
+        except Exception as e:
+            obj["raw_content"] = {}
+
+        try:
+            obj["reason"] = self.reason
+        except Exception as e:
+            obj["reason"] = ""
+
+        try:
+            obj["status_code"] = self.status_code
+        except Exception as e:
+            obj["status_code"] = ""
+
+        try:
+            obj["text"] = self.text
+        except Exception as e:
+            obj["text"] = ""
+
+        try:
+            obj["timestamp_start"] = self.timestamp_start
+        except Exception as e:
+            obj["timestamp_start"] = ""
+
+        try:
+            obj["timestamp_end"] = self.timestamp_end
+        except Exception as e:
+            obj["timestamp_end"] = ""
+
+        # if self.data is not None and isinstance(self.data, dict) and not self.is_json:
+        #     obj["dataMode"] = "urlencoded"
+        #     obj["data"] = [
+        #         dict(key=k, value=v, enabled=True, type="text")
+        #         for k, v in self.data.items()
+        #     ]
+        # elif self.data is not None and not self.is_json:
+        #     obj["dataMode"] = "raw"
+        #     obj["rawModeData"] = str(self.data)
+        # elif self.data is not None and self.is_json:
+        #     obj["dataMode"] = "raw"
+        #     obj["rawModeData"] = json.dumps(self.data, indent=4)
+        # if self.description is not None:
+        #     obj["descriptionFormat"] = "markdown"
+        #     obj["description"] = self.description
+
+        return obj
+
+
 class Folder(object):
     def __init__(self, name, request_no, collection=None):
         """
@@ -356,6 +526,97 @@ class Folder(object):
         obj["name"] = self.name
         obj["order"] = [r.id for r in self._requests]
         return obj
+
+
+class CollectionOfResponses(object):
+    __instance = None
+
+    @staticmethod
+    def getInstance(response_no):
+        """ Static access method. """
+        name = ctx.options.collection_name
+        if CollectionOfResponses.__instance == None:
+            CollectionOfResponses(
+                name=ctx.options.collection_name, response_no=response_no
+            )
+        return CollectionOfResponses.__instance
+
+    def __init__(self, name, response_no, description=None):
+        """
+        A CollectionOfResponses object represents a single postman collection
+        :param name: CollectionOfResponses name
+        :param description: Description for the CollectionOfResponses
+        """
+        # if Collection.__instance == None:
+        if CollectionOfResponses.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            CollectionOfResponses.__instance = self
+            self.id = str(uuid.uuid4())
+            self.response_no = response_no
+            self.name = name
+            self._responses = []
+            self.description = description
+            # print("INITITATLED Collection CLASS WITH NAME:", name)
+
+    # def get_CollectionOfResponses_id(self):
+    #     :return: CollectionOfResponses id
+    #     print("FETCHED CollectionOfResponses:", self.id)
+    #     return self.id
+
+    def add_response(self, response):
+        """
+        Add request to the CollectionOfResponses
+        :param request: Request object
+        :return: None
+        """
+        response = Response(self.name, response, response_no=self.response_no)
+        self._responses.append(response)
+        print("ADDED CollectionOfResponses Response")
+
+    def get_all_responses(self):
+        """
+        Get all requests including those in the folders
+        :return: list of Request objects
+        """
+        responses = list(self._responses)
+        print("Before CollectionOfResponses responses:", self._responses)
+        print("FETCHED CollectionOfResponses responses:", responses)
+        print("New CollectionOfResponses responses:", self._responses)
+        # return responses#, key=attrgetter("id"))
+        return sorted(responses, key=attrgetter("id"))
+
+    def serialize(self):
+        """
+        Serialize CollectionOfResponses object
+        :return: CollectionOfResponses dict
+        """
+        obj = OrderedDict()
+        obj["id"] = self.id
+        obj["name"] = self.name
+        # if self.description is not None:
+        #     obj["description"] = self.description
+        # obj["order"] = [r.id for r in self.responses]
+        responses = self.get_all_responses()
+        obj["responses"] = [r.serialize() for r in responses]
+        print("serialized CollectionOfResponses :", obj)
+        return obj
+
+    def save_to_file(self):
+        """
+        Save Postman CollectionOfResponses to file
+        :return: None
+        """
+        obj = self.serialize()
+        if not os.path.exists("output/{}/CollectionOfResponses/".format(self.name)):
+            os.makedirs("output/{}/CollectionOfResponses/".format(self.name))
+
+        filename = "{file_name}.json".format(**{"file_name": self.name})
+        with open(
+            "output/{}/CollectionOfResponses/".format(self.name) + "/" + filename, "wt"
+        ) as f:
+            json.dump(obj, f, indent=4, ensure_ascii=False)
+        print("save CollectionOfResponses to file:", self.name)
 
 
 addons = [Postman()]
